3f5d5bb53921fbabeda17d7300f464d3e97f8ebb
alann1889 <zurcher1889@gmail.com>
2020-09-28 20:45:15 -0300

    Prime Commit

---
+++ boleto.py
@@ -0,0 +1,10 @@
+/usr/bin/bash: q: command not found
+/usr/bin/bash: w: command not found
+#ademas de tener la opcion de poder pagar medio boleto o entero
+
+#inicializamos las variables
+
+#Cargamos el comienzo y si el pasajero paga medio boleto o entero
+
+/usr/bin/bash: quit: command not found
+#Mostramos el total recaudado y la cantidad de pasajeros
